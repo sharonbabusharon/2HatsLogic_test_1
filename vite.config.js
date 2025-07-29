f58202af0d03918d762958d60d2bf8209e44db63
Sharon Babu <sharonbabu107@gmail.com>
2025-07-29 12:19:54 +0530

    minor updates

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		allowedHosts: ['https://sharonbabusharon.github.io/2HatsLogic_test_1/'],
+	}
 });
