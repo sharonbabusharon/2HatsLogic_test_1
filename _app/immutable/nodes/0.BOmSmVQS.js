5d91a025726f97187591950cefc2023c9f1996df
Sharon Babu <sharonbabu107@gmail.com>
2025-07-29 12:42:40 +0530

    Updates

---
+++ _app/immutable/nodes/0.BOmSmVQS.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import{k as n,l as a,m as p}from"../chunks/BSOCSeBd.js";import{s}from"../chunks/CTi3DyjZ.js";const i=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=n(),t=a(e);s(t,()=>r.children),p(o,e)}export{d as component,u as universal};
